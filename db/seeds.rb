94853082cbfabf2174506257cc36a58ea06a98a4
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2021-12-16 19:47:05 +0100

    controler aibrnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 100 fake restaurants...'
+10.times do
+  flat = Flat.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description:  Faker::Lorem.words(number: 10),
+    price_per_night: Faker::Number.decimal(l_digits: 3),
+    number_of_guests: Faker::Number.non_zero_digit,
+  )
+  flat.save!
+end
+puts 'Finished!'
